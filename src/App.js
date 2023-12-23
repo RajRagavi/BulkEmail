c4aceb30dfaaf988e5899a19f313f67fb1fbf3d6
RagaviRajasekaran <143269843+RajRagavi@users.noreply.github.com>
2023-12-23 17:09:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
       <Routes>
         
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/userlist" element={<UserList />} />
